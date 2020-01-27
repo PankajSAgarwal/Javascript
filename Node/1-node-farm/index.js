7af5ad982bfc2068b395c6f42e1b97510aa77062
Agarwal, Pankaj <2005pank@gmail.com>
2020-01-27 11:47:58 +0530

    1-Node-Farm project\nBuilding the Templates\nFilling The Templates\nUsing Node url module and parsing varibales from url

--- Node/1-node-farm/index.js
+++ Node/1-node-farm/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const http = require("http");
-
+const url = require("url");
 ////////////////////////////////////////////////////////////////////////////////////////////
 ////////*************************** FILES ********************************/////////////////////
 // Blocking , Syncronous way
@@ -40,21 +40,78 @@ const http = require("http");
 ////////*************************** SERVER ********************************////////////////////
 
 // Non - blocking even if synchronous because it is not in a call back and only called once when program starts
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   //console.log(req);
   //res.end('Hello from Server !');
-  const pathName = req.url;
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the overview");
-  } else if (pathName === "/product") {
-    res.end("This is the PRODUCT");
-  } else if (pathName === "/api") {
-    res.writeHead(200, { "Content-Type": "application/json" });
+  //console.log(url.parse(req.url, true));
+  // ES6 destructuring to assign variables query and pathname direcly the value from url.parse
+
+  const { query, pathname } = url.parse(req.url, true);
+
+  // Overview Page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html"
+    });
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join("");
+    //console.log(cardsHtml);
+
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+    res.end(output);
+  }
+  // PRODUCT Page
+  else if (pathname === "/product") {
+    //console.log(query);
+    res.writeHead(200, {
+      "Content-type": "text/html"
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+  }
+  // API PAGE
+  else if (pathname === "/api") {
+    res.writeHead(200, {
+      "Content-Type": "application/json"
+    });
     res.end(data);
-  } else {
+  }
+  // NOT FOUND Page
+  else {
     res.writeHead(404, {
       "Content-type": "text/html",
       "my-own-header": "hello-world"
