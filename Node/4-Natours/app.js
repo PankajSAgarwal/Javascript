f816b954233d91640569ccee776fc59e905276a9
Agarwal, Pankaj <2005pank@gmail.com>
2020-02-16 20:19:36 +0530

    Handling PATCH requests

--- Node/4-Natours/app.js
+++ Node/4-Natours/app.js
@@ -60,9 +60,25 @@ app.get('/api/v1/tours/:id', (req, res) => {
 
   res.status(200).json({
     status: 'success',
-    tour
+    data: { tour }
   });
 });
+
+// PATCH
+// Just for demo purposes , nothing updated 
+app.patch('/api/v1/tours/:id', (req, res) => {
+
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: '<tour updated here>>'
+  });
+})
 
 // POST
 // Express by default doesnt add data to the body of request so we need to use a middleware e.g app.use(express.json())
