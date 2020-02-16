00db098a174bfe57f6e40edc8d0f2912b7465d30
Agarwal, Pankaj <2005pank@gmail.com>
2020-02-16 11:45:14 +0530

    Refactored callback hell to use Promise

--- Node/3-ASYNCHRONOUS-JS/index.js
+++ Node/3-ASYNCHRONOUS-JS/index.js
@@ -4,15 +4,18 @@ const superagent = require('superagent');
 // Callback hell
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed : ${data}`);
-  superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
 
-      if (err) return console.log(err.message);
+  // Promises - Pending , Result .Result can be resolved or rejected .
+  superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then(res => {
+
       console.log(res.body.message);
 
       fs.writeFile('dog-img.txt', res.body.message, err => {
         if (err) return console.log(err.message);
         console.log("Random dog image saved to file");
-      })
-    });
+      });
+    })
+    .catch(err => console.log(err.message));
+
 });
