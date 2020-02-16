7f36f7b56046078ac19d39a3b45f8d3a291c9aa8
Agarwal, Pankaj <2005pank@gmail.com>
2020-02-16 12:33:03 +0530

    Building Promises , readFilePro and writeFilePro and chaining promises

--- Node/3-ASYNCHRONOUS-JS/index.js
+++ Node/3-ASYNCHRONOUS-JS/index.js
@@ -1,21 +1,41 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-// Callback hell
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+// Building Promises
+
+// Building a promise to read a file
+const readFilePro = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I cannot find the specified file.');
+      resolve(data);
+    });
+  });
+}
+
+// Building a promise to write to a file
+
+const writeFilePro = (file, data) => {
+
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, err => {
+      if (err) reject("Cannot write to file .");
+      resolve('success writing to file');
+    });
+  });
+}
+
+readFilePro(`${__dirname}/dog.txt`).then(data => {
   console.log(`Breed : ${data}`);
 
   // Promises - Pending , Result .Result can be resolved or rejected .
-  superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then(res => {
+  return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+}).then(res => {
+  console.log(res.body.message);
+  return writeFilePro('dog-img.txt', res.body.message);
+})
+  .then(() => {
+    console.log("Random dog image saved to file");
+  })
+  .catch(err => console.log(err));
 
-      console.log(res.body.message);
-
-      fs.writeFile('dog-img.txt', res.body.message, err => {
-        if (err) return console.log(err.message);
-        console.log("Random dog image saved to file");
-      });
-    })
-    .catch(err => console.log(err.message));
-
-});
